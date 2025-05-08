3ea732b1e9d3f7157a3e032fb5b22f67119d4279
harjotsingh5 <hsingh79@ivytech.edu>
2025-05-08 17:32:31 -0400

    fixing 'start' button spam

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-<p> Simple Pomodor Timer :)</p>
+<p> Simple Pomodoro Timer :)</p>
 <a href="https://harjot3.github.io/pomodoro-timer/" target="_blank" alt="Link to GitHub Pages">Link to GitHub Pages</a>
--- script.js
+++ script.js
@@ -2,14 +2,17 @@ document.getElementById("start").onclick = beginTimer;
 document.getElementById("pause").onclick = pauseTimer;
 document.getElementById("reset").onclick = resetTimer;
 
-const start_time = 1500; //seconds
+const start_time = 1500; // 25 minutes -> seconds
 var current_time = start_time;
 var stopWatchTimer;
+var stop = true;
 
 function beginTimer() {
-    stopWatch();
-
-    stopWatchTimer = setInterval(stopWatch, 1000);
+    if (stop === true) {
+        stop = false;
+        stopWatch();
+        stopWatchTimer = setInterval(stopWatch, 1000);
+    }
 }
 
 function stopWatch() {
@@ -26,10 +29,12 @@ function stopWatch() {
 
 
 function pauseTimer() {
+    stop = true;
     clearInterval(stopWatchTimer);
 }
 
 function resetTimer() {
+    stop = true;
     clearInterval(stopWatchTimer);
     current_time = start_time;
     document.getElementById("timer").innerHTML = `${Math.floor(current_time / 60)}:${current_time % 60}0`;
